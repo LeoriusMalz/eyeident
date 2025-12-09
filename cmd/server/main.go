80b448c6a416a1e9562dffe7f0370308251b8c95
Лев Мальцев <lev.maltsev@vkteam.ru>
2025-12-09 23:27:14 +0300

    .

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"eyeident/internal/db"
 	"eyeident/internal/server"
 	"log"
 )
@@ -10,10 +11,10 @@ func main() {
 	log.Println("Server running at http://192.168.1.105:8080")
 	log.Println("Server running at http://172.21.55.18:8080")
 
-	//_, err := db.ConnectPostgres()
-	//if err != nil {
-	//	return
-	//}
+	_, err := db.ConnectPostgres()
+	if err != nil {
+		return
+	}
 
 	log.Fatal(srv.Run(":8080"))
 }
